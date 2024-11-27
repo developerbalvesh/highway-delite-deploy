9cc28b81cb981b30e745133901a7c5ee7da1c010
developerbalvesh <developer.balvesh@gmail.com>
2024-11-27 20:48:07 +0530

    deploy 4

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import cors from 'cors';
 const app = express();
 dotenv.config();
 connectDB();
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 // app.use(express.static(path.join(__dirname, './frontend')));
