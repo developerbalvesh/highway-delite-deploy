157b44c9dc4238235bf448031ffa321113920a68
developerbalvesh <developer.balvesh@gmail.com>
2024-11-27 20:34:59 +0530

    deploy 3

--- backend/helper/sendEmail.js
+++ backend/helper/sendEmail.js
@@ -1,32 +1,36 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 export const sendOtpEmail = async (otp, email) => {
+  try {
     const html = `<h3>Please enter below OTP to verify your email:</h3><h1>${otp}</h1>`;
-    const pass = process.env.EMAIL_PASS;
+    const pass = await process.env.EMAIL_PASS;
+    console.log(pass);
     // send mail start
-    const auth = nodemailer.createTransport({
-        service: "gmail",
-        secure: true,
-        port: 465,
-        auth: {
-            user: "pskbalvesh@gmail.com",
-            pass
-        }
+    const auth = await nodemailer.createTransport({
+      service: "gmail",
+      secure: true,
+      port: 465,
+      auth: {
+        user: "pskbalvesh@gmail.com",
+        pass: pass,
+      },
     });
     const receiver = {
-        from: "pskbalvesh@gmail.com",
-        to: email,
-        subject: "Highway Delite OTP verification!",
-        html
+      from: "pskbalvesh@gmail.com",
+      to: email,
+      subject: "Highway Delite OTP verification!",
+      html,
     };
-    auth.sendMail(receiver, (error, emailResponse) => {
-        if (error) {
-            throw error;
-        }
-        else {
-            console.log("success!");
-        }
-        // response.end();
+    await auth.sendMail(receiver, (error, emailResponse) => {
+      if (error) {
+        throw error;
+      } else {
+        console.log("success!");
+      }
+      // response.end();
     });
     // send mail end
+  } catch (error) {
+    console.log(error);
+  }
 };
 //# sourceMappingURL=sendEmail.js.map
